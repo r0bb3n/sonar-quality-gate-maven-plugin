744b043ff4c7a7629bff22457fff82bb26383f35
r0bb3n <r0bb3n@users.noreply.github.com>
2021-02-20 21:38:37 +0100

    GH-31 cover all positive sonar connector flows

--- src/test/java/org/r0bb3n/maven/SonarConnectorProjectStatusTest.java
+++ src/test/java/org/r0bb3n/maven/SonarConnectorProjectStatusTest.java
@@ -22,28 +22,50 @@ import static org.junit.Assert.assertNotNull;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.r0bb3n.maven.model.ProjectStatus;
-import org.r0bb3n.maven.model.Task;
 
 /**
  * Test cases for sonar server communication
  */
 @Log4j2
-public class SonarConnectorTest {
+@RequiredArgsConstructor
+@RunWith(Parameterized.class)
+public class SonarConnectorProjectStatusTest {
+
+  /**
+   * create test data
+   */
+  @Parameterized.Parameters(name = "branch: {0} pr: {1} conditions-size: {2}")
+  public static Iterable<Object[]> generateTestData() {
+    List<Object[]> ret = new ArrayList<>(3);
+    ret.add(new Object[]{null, null, 9});
+    ret.add(new Object[]{"feature/GH-31_increase-test-coverage", null, 5});
+    ret.add(new Object[]{null, "58", 5});
+    return ret;
+  }
 
   // use one wiremock instance for entire test class
   @ClassRule
   public static WireMockClassRule wireMockClassRule = new WireMockClassRule(
       WireMockConfiguration.options().dynamicPort().usingFilesUnderDirectory(
-          "src/test/resources/wiremock/" + SonarConnectorTest.class.getSimpleName()));
+          "src/test/resources/wiremock/" + SonarConnectorProjectStatusTest.class.getSimpleName()));
 
   @Rule
   public WireMockClassRule wireMockRule = wireMockClassRule;
+
+  private final String branch;
+  private final String pullRequest;
+  private final int conditionsSize;
 
   private SonarConnector underTest;
 
@@ -54,30 +76,19 @@ public class SonarConnectorTest {
   public void setUp() throws Exception {
     underTest =
         new SonarConnector(new LogFacade(log), new URL("http://localhost:" + wireMockRule.port()),
-            null, null, null);
+            null, null, "io.github.r0bb3n:sonar-quality-gate-maven-plugin");
   }
 
   /**
-   * Test requesting and parsing of task response
+   * Test requesting and parsing of project status response based on different information
    */
   @Test
-  public void retrieveTask() throws Exception {
-    Task task = underTest.retrieveTask("AXe74ZzR1IiFGsn-Op8X");
-    assertNotNull("no task found", task);
-    assertEquals("Status mismatch", Task.Status.SUCCESS, task.getStatus());
-    assertEquals("Analysis id mismatch", "AXe74Z38wlKnb4b_2mMT", task.getAnalysisId());
-    log.trace(task);
-  }
-
-  /**
-   * Test requesting and parsing of project status response based on an analysis
-   */
-  @Test
-  public void retrieveProjectStatusByAnalysisId() throws Exception {
-    ProjectStatus projectStatus =
-        underTest.retrieveProjectStatusByAnalysisId("AXe74Z38wlKnb4b_2mMT");
+  public void retrieveProjectStatus() throws Exception {
+    ProjectStatus projectStatus = underTest.retrieveProjectStatus(branch, pullRequest);
     assertNotNull("no projectStatus found", projectStatus);
-    assertEquals("Status mismatch", ProjectStatus.Status.OK, projectStatus.getStatus());
+    assertEquals("Unexpected status value", ProjectStatus.Status.OK, projectStatus.getStatus());
+    assertEquals("Unexpected condtions count", conditionsSize,
+        projectStatus.getConditions().size());
     log.trace(projectStatus);
   }
 }
